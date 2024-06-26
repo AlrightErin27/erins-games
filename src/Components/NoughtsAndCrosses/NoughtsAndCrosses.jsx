2c2ab221c89629ddc3b6bda294f1b77d1dba0169
Erin Van Brunt <77031790+AlrightErin27@users.noreply.github.com>
2024-06-25 19:52:55 -0700

    moved changePlayer to avoid skipping turn on no value change

--- src/Components/NoughtsAndCrosses/NoughtsAndCrosses.jsx
+++ src/Components/NoughtsAndCrosses/NoughtsAndCrosses.jsx
@@ -6,23 +6,36 @@ export default function NoughtsAndCrosses() {
   const [board, setBoard] = useState(["", "", "", "", "", "", "", "", ""]);
   const [player, setPlayer] = useState("X");
 
+  const changePlayer = (currPlayer) => {
+    currPlayer === "X" ? setPlayer("O") : setPlayer("X");
+  };
+
   const chooseSquare = (square) => {
-    // console.log("You've chosen:", square);
+    //set the arr called board bu mapping through and looking for a match
     setBoard(
       board.map((val, idx) => {
+        //looking to match clicked squares index with values stored in board arr
+        let newVal;
         if (square === idx && val === "") {
-          return player;
+          //if the idx's match and the current value is empty
+          //change the empty squares val to the value of the current player (X or O)
+          newVal = player;
         } else {
+          //if no match and not empty keep the current value the square already has
+          newVal = val;
+        }
+
+        //if there is a change from above then switch players and setBoard at index to new val
+        if (newVal !== val) {
+          //switching players here makes it so a double click that doesn't change the value also doesn't change the current player
+          changePlayer(player);
+          return newVal;
+        } else {
+          //else the player remains the same and the value stays the same.
           return val;
         }
       })
     );
-
-    if (player === "X") {
-      setPlayer("O");
-    } else {
-      setPlayer("X");
-    }
   };
 
   return (
