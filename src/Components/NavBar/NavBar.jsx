408aced401248a2ac4d5a2d396a15c05489178c0
Erin Van Brunt <77031790+AlrightErin27@users.noreply.github.com>
2024-08-18 23:00:38 -0700

    more dynamic useState in nav bar

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -4,21 +4,31 @@ import "./NavBar.css";
 
 export default function Navbar() {
   const [selectGames, setSelectGames] = useState(false);
+  const [select, setSelect] = useState("");
   const gamesLinkStr = ["Games", "[X]"];
 
   const handleSelectGames = () => {
     console.log("HI");
     setSelectGames(!selectGames);
+    setSelect("games");
   };
 
   return (
     <nav className="NavBar">
       <ul>
-        <CustomLink to="/" className="link">
+        <CustomLink
+          to="/"
+          className={select !== "home" ? "link" : "select-link"}
+          onClick={() => setSelect("home")}
+        >
           Home
         </CustomLink>
 
-        <CustomLink to="/blog" className="link">
+        <CustomLink
+          to="/blog"
+          className={select !== "blog" ? "link" : "select-link"}
+          onClick={() => setSelect("blog")}
+        >
           Blog
         </CustomLink>
 
@@ -32,16 +42,32 @@ export default function Navbar() {
 
           {!selectGames ? null : (
             <>
-              <CustomLink to="concentration1" className="link">
+              <CustomLink
+                to="concentration1"
+                onClick={() => setSelect("game1")}
+                className={select !== "game1" ? "link" : "select-link"}
+              >
                 Concentration1
               </CustomLink>
-              <CustomLink to="noughts-&-crosses" className="link">
+              <CustomLink
+                to="noughts-&-crosses"
+                onClick={() => setSelect("game2")}
+                className={select !== "game2" ? "link" : "select-link"}
+              >
                 Noughts & Crosses
               </CustomLink>
-              <CustomLink to="ice-and-fire" className="link">
+              <CustomLink
+                to="ice-and-fire"
+                onClick={() => setSelect("game3")}
+                className={select !== "game3" ? "link" : "select-link"}
+              >
                 Ice & Fire
               </CustomLink>
-              <CustomLink to="crossword" className="link">
+              <CustomLink
+                to="crossword"
+                onClick={() => setSelect("game4")}
+                className={select !== "game4" ? "link" : "select-link"}
+              >
                 Crossword
               </CustomLink>
             </>
